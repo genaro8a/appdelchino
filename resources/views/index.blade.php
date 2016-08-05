33d9ec1da36e0f45d525bd18c2a10170d4a3f84e
genaro8a <genaro8a@MacBook-Air-de-Saul.local>
2016-08-05 12:30:19 -0500

    PRUEBA
    DE CAMBIO

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 			<div class="top-header">
 				<div class="logo">
 					<a href="index.html"><img src="images/logo.png" alt="" /></a>
-					<p>REMOLQUE</p>
+					<p>APLICACION MOVIL PARA EL REMOLQUE </p>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -14,11 +14,11 @@
 				<h1>INGRESO</h1>
 				{!!Form::open(['route'=>'log.store', 'method'=>'POST'])!!}
 					<div class="form-group">
-						{!!Form::label('correo','Correo:')!!}	
+						{!!Form::label('correo','Correo:')!!}
 						{!!Form::email('email',null,['class'=>'form-control', 'placeholder'=>'Ingresa tu correo'])!!}
 					</div>
 					<div class="form-group">
-						{!!Form::label('contrasena','Contraseña:')!!}	
+						{!!Form::label('contrasena','Contraseña:')!!}
 						{!!Form::password('password',['class'=>'form-control', 'placeholder'=>'Ingresa tu contraseña'])!!}
 					</div>
 					{!!Form::submit('Iniciar',['class'=>'btn btn-primary'])!!}
@@ -34,6 +34,6 @@
 			<li><img src="images/r4.png" alt=""/></li>
 			<li><img src="images/r4.png" alt=""/></li>
 		</ul>
-			
+
 		</div>
-	@endsection	
+	@endsection
